7cd088eaf15fb6a80009520ab9794eb5c82cff5b
georgmaisser <georg.maisser@wunderbyte.at>
2022-11-26 11:16:07 +0100

    third step on subbookings (can add and edit)

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -640,4 +640,14 @@ class renderer extends plugin_renderer_base {
         $data = $data->export_for_template($this);
         return $this->render_from_template('mod_booking/ruleslist', $data);
     }
+
+    /**
+     * Render subbookings list
+     * @param $data array
+     * @return string
+     */
+    public function render_subbookingslist($data) {
+        $data = $data->export_for_template($this);
+        return $this->render_from_template('mod_booking/subbookingslist', $data);
+    }
 }
