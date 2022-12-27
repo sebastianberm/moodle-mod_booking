4379aba0cc6d3e576e88693d4a205f99742b07f0
georgmaisser <georg.maisser@wunderbyte.at>
2022-12-27 17:41:56 +0100

    elective booking option changes

--- classes/booking_option.php
+++ classes/booking_option.php
@@ -1022,6 +1022,15 @@ class booking_option {
     public function enrol_user_coursestart($userid) {
         if ($this->option->enrolmentstatus == 2 OR
             ($this->option->enrolmentstatus < 2 AND $this->option->coursestarttime < time())) {
+
+            // This is a new elective function. We only allow booking in the right order.
+            if ($this->booking->is_elective()) {
+                if (!booking_elective::check_if_allowed_to_inscribe($this, $userid)) {
+                    // mtrace("The user with the userid {$userid} has to finish courses of other booking options first.");
+                    return;
+                }
+            }
+
             $this->enrol_user($userid);
         }
     }
